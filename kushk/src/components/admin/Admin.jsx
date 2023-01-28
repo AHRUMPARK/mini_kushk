16e2b9c03eeb1653c87282e9a82ac0a2f6a3d1ea
arum <arum4523@naver.com>
2023-01-28 16:01:43 +0900

    test2

---
+++ kushk/src/components/admin/Admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Admin() {
+  return (
+    <>
+      <h1>관리자 페이지</h1>
+      <button type="button">메뉴 추가</button>
+      <button type="button">메뉴 수정</button>
+      <button type="button">메뉴 삭제</button>
+    </>
+  );
+}
