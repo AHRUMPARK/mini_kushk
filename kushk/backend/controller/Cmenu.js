3b95a7d21b393406c18135f7bcf70c3901ae5e7b
AHRUMPARK <arum4523@naver.com>
2023-01-31 15:15:28 +0900

    20230131_ar_1

--- kushk/backend/controller/Cmenu.js
+++ kushk/backend/controller/Cmenu.js
@@ -4,10 +4,12 @@ const { Menu } = require('../model/IndexMenu');
 exports.getMenuList = (req, res) => {
   console.log('메뉴 List 불러오기');
   Menu.findAll()
-  .then((result) => {
-    res.send(result)
-  })
-  .catch( err => { throw err });
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
 // 메뉴 추가
@@ -24,44 +26,43 @@ exports.addMenu = async (req, res) => {
   });
 };
 
-  // 메뉴 조회
-  exports.selectMenu = async (req, res) => {
-    console.log('메뉴 조회 : ', req.body);
-    await Menu.findOne({
-      where: {
-        menu_id: req.body.id,
-      },
-    }).then((result) => {
-      console.log(result);
-      res.send(result);
-    });
-  };
+// 메뉴 조회
+exports.selectMenu = async (req, res) => {
+  console.log('메뉴 조회 : ', req.body);
+  await Menu.findOne({
+    where: {
+      menu_id: req.body.id,
+    },
+  }).then((result) => {
+    console.log(result);
+    res.send(result);
+  });
+};
 
-  // 메뉴 수정
-  exports.menuUpdate = async (req, res) => {
-    console.log('메뉴 수정 : ', req.body);
-      await Menu.update(
-        {
-          menu_index: req.body.index,
-          menu_price: req.body.price,
-          menu_img: req.body.img,
-        },
-        {
-          where: { menu_id: req.body.id },
-        }
-      ).then((result) => {
-        console.log(result);
-        res.send('true');
-      });
-  };
+// 메뉴 수정
+exports.menuUpdate = async (req, res) => {
+  console.log('메뉴 수정 : ', req.body);
+  await Menu.update(
+    {
+      menu_index: req.body.index,
+      menu_price: req.body.price,
+      menu_img: req.body.img,
+    },
+    {
+      where: { menu_id: req.body.id },
+    }
+  ).then((result) => {
+    console.log(result);
+    res.send('true');
+  });
+};
 
-  // 메뉴 삭제
-  exports.menuDelete = async(req, res) => {
-        console.log('메뉴 삭제 : ', req.body);
-      await Menu.destroy({
-          where: { menu_id: req.body.id }
-        })
-        .then(() => {
-          res.send(true)
-        });
-  }
+// 메뉴 삭제
+exports.menuDelete = async (req, res) => {
+  console.log('메뉴 삭제 : ', req.body);
+  await Menu.destroy({
+    where: { menu_id: req.body.id },
+  }).then(() => {
+    res.send(true);
+  });
+};
