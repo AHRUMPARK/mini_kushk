a5cca6cba55d2578af8c1835af947d1d24837698
raelulu <raelulu23@gmail.com>
2023-02-01 20:30:15 +0900

    feat: 02/01

--- kushk/src/App.js
+++ kushk/src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 import Coffee from './components/Menu/Coffee';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Admin from './components/admin/Admin';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
