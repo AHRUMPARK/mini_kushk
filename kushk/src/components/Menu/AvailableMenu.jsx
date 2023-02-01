e25904758e990a69e69fd87617f5a424f448549c
AHRUMPARK <arum4523@naver.com>
2023-02-01 20:26:28 +0900

    장바구니 메뉴 읽는 문제 수정

--- kushk/src/components/Menu/AvailableMenu.jsx
+++ kushk/src/components/Menu/AvailableMenu.jsx
@@ -3,11 +3,12 @@ import MenuItem from './MenuItem/MenuItem';
 import classes from './AvailableMenu.module.css';
 
 //데이터베이스에서 데이터 가져오기
+// MenuItem 컴포넌트에 props 전달
 const AvailableMenu = ({ menuList }) => {
   console.log(menuList);
 
   // id = 품목
-  // name = 상품명
+  // name = 상품명 ex. 커피, 논커피
   // price = 가격
   // description = 설명
   return (
